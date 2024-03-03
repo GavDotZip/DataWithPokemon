cc0d399d722a3cd09fe82bdbea5ffa09a6abe538
GavDotZip <gavdotzip@gmail.com>
2024-03-03 21:49:30 +0000

    commit

--- index.R
+++ index.R
@@ -1,1 +1,6 @@
-pokemon <- "Pokemon.csv"
+library(tidyverse) # metapackage of all tidyverse packages
+library(RColorBrewer)
+library(GGally)
+
+df <- read.csv("Pokemon.csv", header=T, row.names = 2)
+head(df)
