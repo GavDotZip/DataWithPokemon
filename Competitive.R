d9e153948250ba7f591bee1bbf0c116ae49c0356
GavDotZip <gavdotzip@gmail.com>
2024-04-08 23:15:28 +0100

    commit

--- Competitive.R
+++ Competitive.R
@@ -7,3 +7,6 @@ library(GGally)
 # Read the CSV file into a data frame
 df <- read.csv("Pokemon.csv", header = TRUE, row.names = 2)
 head(df)
+
+# Insert Current Competitive Team as of April 2024
+compTeam <- c("Blastoise", "Ampharos", "Gengar", "Garchomp", "Venusaur", "Torkoal")
