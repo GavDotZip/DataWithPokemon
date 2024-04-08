069cda99946c9d213f29c0b6f7c346659f711b0c
GavDotZip <gavdotzip@gmail.com>
2024-04-08 23:05:20 +0100

    commit

--- Competitive.R
+++ Competitive.R
@@ -1,2 +1,9 @@
 # Script to Analyze my Competitive Team
 
+library(tidyverse)
+library(RColorBrewer)
+library(GGally)
+
+# Read the CSV file into a data frame
+df <- read.csv("Pokemon.csv", header = TRUE, row.names = 2)
+head(df)
